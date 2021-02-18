601faba4ba7af9e4343b8451827f01389ab16e9e
Kuddleman <edl1155@gmail.com>
2021-02-17 19:49:34 -0800

    add tester class

--- DepartmentTester.java
+++ DepartmentTester.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class DepartmentTester {
+
+    public static void main(String[] args) {
+        Department salesDepartment = new Department();
+
+        for (int i = 0; i < 2; i++) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter the employee id:");
+            int id = Integer.parseInt(scan.nextLine());
+            System.out.println("What type? full, part or intern");
+            String type = scan.nextLine();
+        }
+    }
 }
