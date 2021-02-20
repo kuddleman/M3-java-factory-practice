278aec4c452afbd997903ff0481d8bd9eb3a07b6
Kuddleman <edl1155@gmail.com>
2021-02-19 17:15:56 -0800

    instantiate objects in AddressTester

---
+++ AddressTester.java
@@ -0,0 +1,8 @@
+public class AddressTester {
+
+    public static void main(String[] args) {
+        AddressOld address1 = new AddressOld("50", "Phelan Ave", "San Francisco", "CA", "94112", "0000", "USA");
+        AddressOld address2 = new AddressOld("924", "Presidio Ave", "San Francisco", "CA", "94115", "USA");
+    }
+
+}
